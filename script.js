9c347f5b59deddcdfcec941a79d21154631f3bf8
fabianafk <70484512+fabianafk@users.noreply.github.com>
2021-03-07 13:01:00 -0300

    transformação funcionando, mudança do titulo do resultado funcionando - inicio do titulo nao capitalizado

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ fundo tarde
 background-image: url(img/afternoon.jpg);
 background-size: 1920px 970px;
 */
+
+var input = document.querySelector('.input-titulo')
+var resultText = document.querySelector('.result-text')
+var minuscula = document.querySelector('#min-btn')
+var maiuscula = document.querySelector('#mai-btn')
+var resultTitle = document.querySelector('.result-title')
+
+minuscula.addEventListener('click', () => {
+    var resultLower = input.value.toLowerCase();
+    resultText.innerHTML = resultLower;
+    resultTitle.innerHTML = 'minúsculas';
+})
+
+maiuscula.addEventListener('click', () => {
+    var resultUpper = input.value.toUpperCase();
+    resultText.innerHTML = resultUpper;
+    resultTitle.innerHTML = 'maiúsculas';
+})
